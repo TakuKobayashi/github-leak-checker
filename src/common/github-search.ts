837f750dc01925bf99c8a7f1e03181228e073882
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-01-30 14:40:02 +0900

    execute format

--- src/common/github-search.ts
+++ src/common/github-search.ts
@@ -1,27 +1,27 @@
-import { Octokit } from "@octokit/rest";
+import { Octokit } from '@octokit/rest';
 const octokit = new Octokit();
 
 const LIMIT_QUERY_WORDS = 256;
-const SEARCH_JOIN_WORD = " OR ";
+const SEARCH_JOIN_WORD = ' OR ';
 
 export async function searchCommits(commitHashes: string[] | string) {
   const flatCommitHashes = [commitHashes].flat();
   const searchQueryHashes = sliceLimitHashes(flatCommitHashes);
-  const searchResults = await octokit.search.commits({q: searchQueryHashes.join(SEARCH_JOIN_WORD)})
+  const searchResults = await octokit.search.commits({ q: searchQueryHashes.join(SEARCH_JOIN_WORD) });
   return searchResults.data;
 }
 
 export async function searchAllCommitItems(commitHashes: string[] | string) {
   const flatCommitHashes = [commitHashes].flat();
-  const searchAllResults = []
-  while(flatCommitHashes.length > 0){
+  const searchAllResults = [];
+  while (flatCommitHashes.length > 0) {
     const searchQueryHashes = sliceLimitHashes(flatCommitHashes);
-    for(const searchQueryHash of searchQueryHashes){
+    for (const searchQueryHash of searchQueryHashes) {
       const removeIndex = flatCommitHashes.indexOf(searchQueryHash);
-      flatCommitHashes.splice(removeIndex, 1)
+      flatCommitHashes.splice(removeIndex, 1);
     }
-    const searchResults = await octokit.search.commits({q: searchQueryHashes.join(SEARCH_JOIN_WORD)})
-    for(const item of searchResults.data.items){
+    const searchResults = await octokit.search.commits({ q: searchQueryHashes.join(SEARCH_JOIN_WORD) });
+    for (const item of searchResults.data.items) {
       searchAllResults.push(item);
     }
   }
@@ -31,9 +31,9 @@ export async function searchAllCommitItems(commitHashes: string[] | string) {
 function sliceLimitHashes(commitHashes: string[]): string[] {
   const searchQueryHashes = [];
   let wordCount = 0;
-  for(const commiteHash of commitHashes){
+  for (const commiteHash of commitHashes) {
     const willSumWordCount = wordCount + commiteHash.length + SEARCH_JOIN_WORD.length;
-    if(willSumWordCount > LIMIT_QUERY_WORDS){
+    if (willSumWordCount > LIMIT_QUERY_WORDS) {
       break;
     }
     wordCount = willSumWordCount;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { program } from 'commander';
-import { sync as readPkgUpSync  } from 'read-pkg-up';
-import checkAction from "./command/check";
+import { sync as readPkgUpSync } from 'read-pkg-up';
+import checkAction from './command/check';
 
-const manifest = readPkgUpSync({cwd: require.resolve('.')});
+const manifest = readPkgUpSync({ cwd: require.resolve('.') });
 
 /**
  * Set global CLI configurations
@@ -14,7 +14,5 @@ program.storeOptionsAsProperties(false);
  */
 program.version(manifest ? manifest.packageJson.version : 'unknown', '-v, --version', 'output the current version');
 
-program.command('check')
-.description('check the same program projects from Github')
-.action(checkAction);
+program.command('check').description('check the same program projects from Github').action(checkAction);
 program.parse(process.argv);
