352c5ccddf9e411e1412f0602e79f4707b42bd4c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-01-30 10:53:09 +0900

    add git command scripts

---
+++ src/lib/git-commander.ts
@@ -0,0 +1,16 @@
+import simpleGit, { SimpleGit, LogResult, DefaultLogFields } from 'simple-git';
+
+const git: SimpleGit = simpleGit();
+
+async function execGitLogs(): Promise<LogResult<DefaultLogFields>> {
+  return git.log();
+}
+
+async function loadAllGitLogs() {
+  const resultGitlogs = await execGitLogs();
+  return resultGitlogs.all;
+}
+
+async function execGitRemotes() {
+  return git.getRemotes(true);
+}
