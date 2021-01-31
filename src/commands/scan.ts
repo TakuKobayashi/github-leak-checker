97ceba92ecf4c36f21da51dd9750aba7a02af17d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-01-31 22:55:51 +0900

    execute format

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -1,6 +1,15 @@
 import { scanFromCurrent } from '../components/scan';
+import * as chank from 'chalk';
 
 export default async (options: any): Promise<void> => {
-  const scannedResult = await scanFromCurrent();
-  console.log(scannedResult.length);
+  const scannedRepositories = await scanFromCurrent();
+  for (const repository of scannedRepositories) {
+    console.log(
+      [
+        'repository name:' + repository.name,
+        'url:' + chank.cyanBright(repository.html_url),
+        'repository owner name:' + repository.owner.login,
+      ].join(' '),
+    );
+  }
 };
