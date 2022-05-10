7040884d8e2845f93ad90a192729aa4e63b1afb8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-11 03:15:29 +0900

    fix new style

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -1,14 +1,14 @@
 import { scanRepositories } from '../components/scan';
-import { green, magenta, cyanBright } from 'chalk';
+import chalk from 'chalk';
 
 export default async (options: any): Promise<void> => {
   const scannedRepositories = await scanRepositories();
   for (const repository of scannedRepositories) {
     console.log(
       [
-        green('repositoryName') + ':' + magenta(repository.name),
-        green('url') + ':' + cyanBright(repository.html_url),
-        green('repositoryOwnerName') + ':' + magenta(repository.owner.login),
+        chalk.green('repositoryName') + ':' + chalk.magenta(repository.name),
+        chalk.green('url') + ':' + chalk.cyanBright(repository.html_url),
+        chalk.green('repositoryOwnerName') + ':' + chalk.magenta(repository.owner.login),
       ].join(','),
     );
   }
